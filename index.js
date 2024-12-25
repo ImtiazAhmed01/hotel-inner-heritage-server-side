c84eb93168c96c6283bb4d113496887e39bfe494
ImtiazAhmed01 <imtiazahm21@gmail.com>
2024-12-24 22:57:07 -0700

    initializing

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('SIMPLE CRUD IS RUNNING')
+})
+app.listen(port, () => {
+    console.log(`SIMPLE crud is running on port: ${port}`)
+})
